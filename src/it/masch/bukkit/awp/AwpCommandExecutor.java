d714a9a4b687ec3c69dbeb3e79d0da04e241d4b4
Dragobar <jan.czogalla@gmx.de>
2012-07-15 04:46:27 +0200

    Started rework using TelePlusPlus
    - renamed Main to AWP (also changed plugin.yml)
    - refactored doWarp, using the TelePlusPlus Plugin
    - refactored finding warp points
    TODO:
    - changing/setting player direction/view angle
    - making warp points private/public and manage inviting

--- src/it/masch/bukkit/awp/AwpCommandExecutor.java
+++ src/it/masch/bukkit/awp/AwpCommandExecutor.java
@@ -1,114 +1,163 @@
 package it.masch.bukkit.awp;
 
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.entity.Player;
-import java.util.logging.Logger;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.PluginsCommand;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class AwpCommandExecutor implements CommandExecutor
-{
-    private JavaPlugin plugin;
-    private FileConfiguration config; 
-    private ConfigurationSection warps;
+public class AwpCommandExecutor implements CommandExecutor {
+	private final JavaPlugin plugin;
+	private final FileConfiguration config;
+	private ConfigurationSection warps;
+	private final Plugin tpp;
 
-    public AwpCommandExecutor(JavaPlugin plugin) 
-    {
-        this.plugin = plugin;
-        this.config = plugin.getConfig();
-        this.warps  = this.config.getConfigurationSection("warps");
-        if(this.warps == null) {
-            this.warps = this.config.createSection("warps");
-        }
-    }
-    
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) 
-    {
-        Player player = null;
-        if (sender instanceof Player) {
-            player = (Player) sender;
-        } else {
-            return false;
-        }
+	public AwpCommandExecutor(JavaPlugin plugin) {
+		this.plugin = plugin;
+		this.config = plugin.getConfig();
+		this.warps = this.config.getConfigurationSection("warps");
+		if (this.warps == null) {
+			this.warps = this.config.createSection("warps");
+		}
+		this.tpp = plugin.getServer().getPluginManager()
+				.getPlugin("TelePlusPlus");
+		if (this.tpp == null || !this.tpp.isEnabled()) {
+			throw new RuntimeException(
+					"TelePlusPlus not active on this server!");
+		}
+	}
 
-        if(args.length < 1) { 
-            sender.sendMessage("help?!");
-        } else {
-            if(args[0].equalsIgnoreCase("create")) {
-                this.doCreate(player, args);
-            }
-            else {
-                this.doWarp(player, args); 
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args) {
+		Player player = null;
+		if (sender instanceof Player) {
+			player = (Player) sender;
+		} else {
+			return false;
+		}
 
-            }
-        }
+		if (args.length < 1) {
+			sender.sendMessage("help?!");
+		} else {
+			if (args[0].equalsIgnoreCase("create")) {
+				this.doCreate(player, args);
+			} else {
+				this.doWarp(player, args);
 
-        return true;                             
-    }
+			}
+		}
 
-    public void doWarp(Player pl, String[] args) 
-    {
-        String wp = args[0];
-        Player owner = null;
-        if(wp.indexOf(".") != -1) {
-            String ownerName = wp.substring(0, wp.indexOf("."));
-            owner = this.plugin.getServer().matchPlayer(ownerName).get(0);
-            wp = wp.substring(wp.indexOf(".")+1);
-        }
-        if(owner == null) {
-            owner = pl;
-        }
-        try { 
-            wp = owner.getName().toLowerCase() + "." + wp;
-            if(owner == pl) {
-                if(pl.hasPermission("awp.warp.own") && this.warps.contains(wp)) {
-                    pl.teleport(new WarpPoint(this.plugin.getServer(), this.warps.getString(wp)));
-                    return;
-                }
-                pl.sendMessage("looking for public warp points");
-            } else {
-                if(pl.hasPermission("awp.warp.public") && this.warps.contains(wp)) {
-                    pl.teleport(new WarpPoint(this.plugin.getServer(), this.warps.getString(wp)));
-                    return;
-                }
-                pl.sendMessage("looking for public warp point " + wp);
-            }
-        } catch(Exception e) {
+		return true;
+	}
 
-        }
-    }
+	public void doWarp(Player pl, String[] args) {
+		if (!pl.hasPermission("awp.warp.own")
+				&& !pl.hasPermission("awp.warp.public")) {
+			pl.sendMessage("no permission to warp, buddy");
+		}
 
-    public void doCreate(Player pl, String[] args) 
-    {
-        if(!pl.hasPermission("awp.create")) {
-            pl.sendMessage("missing permissions! (awp.create)");
-            return;
-        }
-        String name = "";
-        if(args.length <= 1) {
-            pl.sendMessage("please provide a name for this warp point");
-            pl.sendMessage("usage: /awp create <name>");
-            return;
-        } 
-        name = pl.getName().toLowerCase() + "." + args[1];
-        if(this.warps.contains(name)) {
-            pl.sendMessage("this warp point already exists.");
-            pl.sendMessage("please use /awp set to change a warp points location.");
-        }
-        WarpPoint wp = new WarpPoint(pl.getLocation());
-        String wps   = wp.toString();
-        this.warps.set(name, wps); 
+		String wp = args[0];
+		String owner = null;
+		String player = pl.getName();
+		if (wp.indexOf(".") != -1) {
+			String ownerName = wp.substring(0, wp.indexOf("."));
+			for (String name : warps.getKeys(false)) {
+				if (name.toLowerCase().startsWith(ownerName)) {
+					owner = name;
+					wp = wp.substring(wp.indexOf(".") + 1);
+					break;
+				}
+			}
+		}
 
-        pl.sendMessage("warp created");
-        this.saveConfig();
-    }
+		String wpOwner = "";
 
-    public void saveConfig()
-    {
-        this.plugin.saveConfig();
-    }
+		while (true) {
+			if ((owner == null || owner.equals(player))
+					&& pl.hasPermission("awp.warp.own")
+					&& warps.contains(player + "." + wp)) {
+				wpOwner = player;
+				break;
+			}
+
+			if (player.equals(owner)) {
+				if (pl.hasPermission("awp.warp.own"))
+					pl.sendMessage("sorry, warp point not found");
+				else
+					pl.sendMessage("no permission to warp, buddy");
+				return;
+			}
+
+			if (!pl.hasPermission("awp.warp.public")) {
+				pl.sendMessage("no permission to warp, buddy");
+				return;
+			}
+
+			if (owner == null) {
+				for (String name : warps.getKeys(false)) {
+					if (warps.contains(name + "." + wp)) {
+						// TODO (isPublic y/n)
+						wpOwner = name;
+						break;
+					}
+				}
+				pl.sendMessage("sorry, warp point not found");
+				return;
+			}
+
+			if (!warps.contains(owner + "." + wp)) {
+				pl.sendMessage("sorry, warp point not found");
+				return;
+			}
+
+			// TODO (ispublic y/n)
+			wpOwner = owner;
+			break;
+		}
+		try {
+			WarpPoint warp = new WarpPoint(this.plugin.getServer(),
+					this.warps.getString(wpOwner + "." + wp));
+			String[] argsToTPP = { warp.getWorld().getName(), "" + warp.getX(),
+					"" + warp.getY(), "" + warp.getZ() };
+			this.tpp.onCommand(pl, new PluginsCommand("tp"), "tp", argsToTPP);
+			// TODO direction/view angle
+
+		} catch (Exception e) {
+			pl.sendMessage("broken data, Junge!");
+		}
+	}
+
+	public void doCreate(Player pl, String[] args) {
+		if (!pl.hasPermission("awp.create")) {
+			pl.sendMessage("missing permissions! (awp.create)");
+			return;
+		}
+		String name = "";
+		if (args.length <= 1) {
+			pl.sendMessage("please provide a name for this warp point");
+			pl.sendMessage("usage: /awp create <name>");
+			return;
+		}
+		name = pl.getName().toLowerCase() + "." + args[1];
+		if (this.warps.contains(name)) {
+			pl.sendMessage("this warp point already exists.");
+			pl.sendMessage("please use /awp set to change a warp points location.");
+		}
+		WarpPoint wp = new WarpPoint(pl.getLocation());
+		String wps = wp.toString();
+		this.warps.set(name, wps);
+
+		pl.sendMessage("warp created");
+		this.saveConfig();
+	}
+
+	public void saveConfig() {
+		this.plugin.saveConfig();
+	}
 
 }
--- src/it/masch/bukkit/awp/WarpPoint.java
+++ src/it/masch/bukkit/awp/WarpPoint.java
@@ -3,41 +3,34 @@ package it.masch.bukkit.awp;
 import org.bukkit.Server;
 import org.bukkit.Location;
 
-class WarpPoint extends Location 
-{
-    public WarpPoint(Location loc) 
-    {
-        super(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(), loc.getPitch());
-    }
+class WarpPoint extends Location {
+	public WarpPoint(Location loc) {
+		super(loc.getWorld(), loc.getX(), loc.getY(), loc.getZ(), loc.getYaw(),
+				loc.getPitch());
+	}
 
-    public WarpPoint(Server srv, String str) throws Exception
-    {
-        super(null, 0, 0, 0, 0, 0);
-        String[] params = str.split(",");
-        if(params.length != 6) {
-            throw new Exception("invalid data format");
-        }
-        Location loc = new Location(
-                srv.getWorld(params[0]),
-                Double.parseDouble(params[1]),  // X 
-                Double.parseDouble(params[2]),  // Y
-                Double.parseDouble(params[3]),  // Z
-                Float.valueOf(params[4]).floatValue(),    // yaw
-                Float.valueOf(params[5]).floatValue()     // pitch
-        );
-        this.setWorld(loc.getWorld());
-        this.add(loc);
-        this.setYaw(loc.getYaw());
-        this.setPitch(loc.getPitch());
-    }
+	public WarpPoint(Server srv, String str) throws Exception {
+		super(null, 0, 0, 0, 0, 0);
+		String[] params = str.split(",");
+		if (params.length != 6) {
+			throw new Exception("invalid data format");
+		}
+		Location loc = new Location(srv.getWorld(params[0]),
+				Double.parseDouble(params[1]), // X
+				Double.parseDouble(params[2]), // Y
+				Double.parseDouble(params[3]), // Z
+				Float.valueOf(params[4]).floatValue(), // yaw
+				Float.valueOf(params[5]).floatValue() // pitch
+		);
+		this.setWorld(loc.getWorld());
+		this.add(loc);
+		this.setYaw(loc.getYaw());
+		this.setPitch(loc.getPitch());
+	}
 
-    public String toString()
-    {
-        return "" + this.getWorld().getName() + "," + 
-                    this.getX() + "," + 
-                    this.getY() + "," + 
-                    this.getZ() + "," + 
-                    this.getYaw() + "," + 
-                    this.getPitch(); 
-    }
+	public String toString() {
+		return "" + this.getWorld().getName() + "," + this.getX() + ","
+				+ this.getY() + "," + this.getZ() + "," + this.getYaw() + ","
+				+ this.getPitch();
+	}
 }
