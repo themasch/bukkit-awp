aea196c8df259151c187b69fd247d2c3dcf6d99a
Dragobar <jan.czogalla@udo.edu>
2014-01-06 22:46:03 +0100

    PluginDescriptionFile in Use

--- src/main/java/it/masch/bukkit/awp/AWP.java
+++ src/main/java/it/masch/bukkit/awp/AWP.java
@@ -1,19 +1,21 @@
 package it.masch.bukkit.awp;
 
 import java.util.logging.Logger;
-import org.bukkit.plugin.java.JavaPlugin;
+
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class AWP extends JavaPlugin
 {
+
     private Logger log = Logger.getLogger("Minecraft");
     private CommandExecutor cmd;
 
     public void onEnable()
     {
-        //PluginDescriptionFile desc = getDescription();
-        //log.info("[" + desc.getName() + "#" + desc.getVersion() + "] enabled");
+        PluginDescriptionFile desc = getDescription();
+        log.info("[" + desc.getName() + "#" + desc.getVersion() + "] enabled");
         log.info("AWP ENABLED");
         cmd = new AwpCommandExecutor(this);
         getCommand("awp").setExecutor(cmd);
@@ -21,8 +23,8 @@ public class AWP extends JavaPlugin
 
     public void onDisable()
     {
-        //PluginDescriptionFile desc = getDescription();
-        //log.info("[" + desc.getName() + "#" + desc.getVersion() + "] disabled");
+        PluginDescriptionFile desc = getDescription();
+        log.info("[" + desc.getName() + "#" + desc.getVersion() + "] disabled");
     }
 
 }
